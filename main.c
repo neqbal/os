65e2d77ab316f983a04fd28f342bb0f4a4158b7c
neqbal <nooraineqbal@gmail.com>
2024-06-13 06:29:41 +0530

    keyboard_input

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 int main() {
-    uart_put_char("Woo\n");
+    uart_put_char("abc\n");
+    
     uart_get_char();
 }
--- ns16550a.s
+++ ns16550a.s
@@ -7,35 +7,46 @@
 uart_get_char:
     .cfi_startproc
     addi sp,sp,-16
-    sd s0,8(sp)
+    sd ra, 8(sp)
+    sd s0,0(sp)
     addi s0,sp,16
 
     la s1, __uart_base_addr
-    
+    lw a0, 0(s1)
+    call uart_put_char
+
+    ld ra, 8(sp)
+    ld s0, 0(sp)
+    addi sp, sp , 16
+    ret
     .cfi_endproc
 
 uart_put_char:
     .cfi_startproc
     #prologue
     addi sp,sp,-32 
-    sd fp,24(sp)
-    addi fp,sp,32
+    sd s0, 0(sp)
+    addi s0,sp,32
 
     mv a5,a0
     sb a5,-17(s0)
     la s1, __uart_base_addr
     mv s2, a0
-    #addi s3, s2, 14
+    
+    li t1, 255
+    bgt s2, t1, str
+    sb s2, 0(s1)
+    j end
 
-1:  
+str:  
     lb s3, 0(s2)
     sb s3, 0(s1)
-    addi s2, s2, 1
+    addi s2, s2,1
     lb t1, 0(s2)
-    bne t1, zero, 1b
+    bne t1, zero, str
 
-    #epilogue
-    ld s0, 24(sp)
+end:    #epilogue
+    ld s0, 0(sp)
     addi sp, sp, 32
     ret    
 
